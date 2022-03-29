58f97e93cc5054abf093ac7e0d3092ae3266b1a6
hackeromair <omair.mohammed031@gmail.com>
2022-03-29 17:40:52 +0530

    updating blazor files

---
+++ SuperHeroDB1/Client/Services/ISuperHeroService.cs
@@ -0,0 +1,21 @@
+﻿using SuperHeroDB1.Server.SuperHeroDB1.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroDB1.Client.Services
+{
+    public interface ISuperHeroService
+    {
+        event Action OnChange;
+        List<Comic> Comics { get; set; }
+        List<SuperHero> Heroes { get; set; }
+        Task<List<SuperHero>> GetSuperHeroes();
+        Task GetComics();
+        Task<SuperHero> GetSuperHero(int id);
+        Task<List<SuperHero>> CreateSuperHero(SuperHero hero);
+        Task<List<SuperHero>> UpdateSuperHero(SuperHero hero, int id);
+        Task<List<SuperHero>> DeleteSuperHero(int id);
+    }
+}
---
+++ SuperHeroDB1/Shared/Comic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroDB1.Server.SuperHeroDB1.Shared
+{
+    public class Comic
+    {
+        public int Id { get; set; } = 1;
+        public string Name { get; set; }
+    }
+}
